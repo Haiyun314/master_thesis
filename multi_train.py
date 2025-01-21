f4d4bf31560334f12abdaff3e0f0ec0127bd46aa
Haiyun314 <haiyunning@gmx.com>
2025-01-21 16:04:11 +0100

    reduced middle state

--- multi_train.py
+++ multi_train.py
@@ -14,13 +14,13 @@ def unit_nn(input_shape, output_shape, units, layers_per_segment):
 
 def multi_shot(x, y, layers_per_segment, units, epochs, learning_rate):
     # Randomly initialize trainable middle states
-    middle_state0 = tf.Variable(tf.random.normal([x.shape[0], units], dtype=tf.float32), trainable=True)
-    middle_state1 = tf.Variable(tf.random.normal([x.shape[0], units], dtype=tf.float32), trainable=True)
+    middle_state0 = tf.Variable(tf.random.normal([x.shape[0], 1], dtype=tf.float32), trainable=True)
+    middle_state1 = tf.Variable(tf.random.normal([x.shape[0], 1], dtype=tf.float32), trainable=True)
 
     # Instantiate each segment
-    segment1 = unit_nn(x.shape[1:], middle_state0.shape[1], units=units, layers_per_segment=layers_per_segment) # shape x = (1, ), y = 16
-    segment2 = unit_nn(middle_state0.shape[1:], middle_state1.shape[1], units=units, layers_per_segment=layers_per_segment) # shape x = (16, ), y = 16
-    segment3 = unit_nn(middle_state1.shape[1:], y.shape[1], units=units, layers_per_segment=layers_per_segment) # shape x = (16, ), y = 1
+    segment1 = unit_nn(x.shape[1:], 16, units=units, layers_per_segment=layers_per_segment) # shape x = (1, ), y = 16
+    segment2 = unit_nn(16, 16, units=units, layers_per_segment=layers_per_segment) # shape x = (16, ), y = 16
+    segment3 = unit_nn(16, y.shape[1], units=units, layers_per_segment=layers_per_segment) # shape x = (16, ), y = 1
 
     # Combine segments into a single unified model
     input_layer = tf.keras.layers.Input(shape=x.shape[1:])
@@ -38,14 +38,14 @@ def multi_shot(x, y, layers_per_segment, units, epochs, learning_rate):
         # Train segment 1
         with tf.GradientTape() as tape1:
             intermediate1_output = segment1(x, training=True)
-            loss1 = mse_loss(intermediate1_output, middle_state0)
+            loss1 = mse_loss(tf.reduce_sum(intermediate1_output, axis= -1), middle_state0)
         gradients1 = tape1.gradient(loss1, segment1.trainable_variables + [middle_state0])
         optimizer.apply_gradients(zip(gradients1, segment1.trainable_variables + [middle_state0]))
 
         # Train segment 2
         with tf.GradientTape() as tape2:
             intermediate2_output = segment2(intermediate1_output, training=True)
-            loss2 = mse_loss(intermediate2_output, middle_state1)
+            loss2 = mse_loss(tf.reduce_sum(intermediate2_output, axis= -1), middle_state1)
         gradients2 = tape2.gradient(loss2, segment2.trainable_variables + [middle_state1])
         optimizer.apply_gradients(zip(gradients2, segment2.trainable_variables + [middle_state1]))
 
