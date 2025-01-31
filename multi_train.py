0278312c5b49bdbbddbedaf4b3e3fa5e189d6f4a
Haiyun314 <haiyunning@gmx.com>
2025-01-31 11:13:42 +0100

    Add training mode selection
    User can choose between training the whole model directly or training a segment-based network.

--- multi_train.py
+++ multi_train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import os 
+import time
 
 def unit_nn(input_shape, output_shape, units, layers_per_segment):
     input_layer = tf.keras.layers.Input(shape=input_shape)
@@ -12,7 +13,7 @@ def unit_nn(input_shape, output_shape, units, layers_per_segment):
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
     return model
 
-def multi_shot(x, y, layers_per_segment, number_of_segments, units, epochs, learning_rate):
+def multi_shot(x, y, layers_per_segment, number_of_segments, units, epochs, learning_rate, train_full_model):
     # Randomly initialize trainable middle states
     middle_state = [tf.Variable(tf.random.normal([x.shape[0], units], dtype=tf.float32), trainable=True) for _ in range(number_of_segments)]
     all_states = [x] + middle_state + [y]
@@ -36,25 +37,39 @@ def multi_shot(x, y, layers_per_segment, number_of_segments, units, epochs, lear
     loss_record = []
 
     for epoch in range(epochs):
-        segments_loss = 0
-        for i in range(0, len(segments)):
+        if train_full_model:
+            start_timer = time.perf_counter()
             with tf.GradientTape() as tape:
-                intermediate_output = segments[i](all_states[i], training=True)
-                loss = mse_loss(intermediate_output, all_states[i + 1])
-
-            # Note: the place of middle_state in the trainable variables list is important
-            if i == len(segments) - 1:
-                gradients = tape.gradient(loss, segments[i].trainable_variables)
-                optimizer.apply_gradients(zip(gradients, segments[i].trainable_variables))
-            else:
-                gradients = tape.gradient(loss, segments[i].trainable_variables + [middle_state[i]])
-                optimizer.apply_gradients(zip(gradients, segments[i].trainable_variables + [middle_state[i]]))
-            segments_loss += loss
+                full_loss = mse_loss(full_model(x), y)
+            gradients = tape.gradient(full_loss, full_model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, full_model.trainable_variables))
+            end_timer = time.perf_counter()
             del tape
+            time_consume = end_timer - start_timer
+            loss_record.append([time_consume, full_loss])
+            print(f'full_loss={full_loss}, time comsume = {time_consume}, epoch = {epoch} \n')
+        else:
+            start_timer = time.perf_counter()
+            segments_loss = 0
+            for i in range(0, len(segments)):
+                with tf.GradientTape() as tape:
+                    intermediate_output = segments[i](all_states[i], training=True)
+                    loss = mse_loss(intermediate_output, all_states[i + 1])
 
-        full_loss = mse_loss(full_model(x), y)
-        loss_record.append([segments_loss, full_loss])
-        print(f"Epoch {epoch + 1}/{epochs}, Loss of segments: {segments_loss.numpy()}, Full model loss: {full_loss.numpy()}")
+                # Note: the place of middle_state in the trainable variables list is important
+                if i == len(segments) - 1:
+                    gradients = tape.gradient(loss, segments[i].trainable_variables)
+                    optimizer.apply_gradients(zip(gradients, segments[i].trainable_variables))
+                else:
+                    gradients = tape.gradient(loss, segments[i].trainable_variables + [middle_state[i]])
+                    optimizer.apply_gradients(zip(gradients, segments[i].trainable_variables + [middle_state[i]]))
+                segments_loss += loss
+                del tape
+            end_timer = time.perf_counter()
+            time_consume = end_timer - start_timer
+            full_loss = mse_loss(full_model(x), y)
+            loss_record.append([segments_loss, full_loss])
+            print(f"Epoch {epoch + 1}/{epochs}, Loss of segments: {segments_loss.numpy()}, Full model loss: {full_loss.numpy()}, time_consume : {time_consume} \n")
 
     return full_model, loss_record
 
@@ -72,27 +87,29 @@ def plot(x, y, predictions, name, loss: bool = True):
     plt.savefig(f"images/{name[2]}.png")
     plt.cla()
 
-def main():
-    x = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1).astype(np.float32) 
-    y = np.sin(x).reshape(-1, 1).astype(np.float32)  
-
+def main(args):
     model, loss_record = multi_shot(
-        x = x, 
-        y = y,
-        layers_per_segment=2,
-        number_of_segments=2,
-        units=16,
-        epochs=5000,
-        learning_rate=0.001
+        **args
     )
-
-    predictions = model.predict(x)
-    plot(x, y, predictions,
-         ["True sin(x)", "Predicted sin(x)", "results"], loss=False)
+    predictions = model.predict(args['x'])
+    plot(args['x'], args['y'], predictions,
+         ["True sin(x)", "Predicted sin(x)", "train_full_results"], loss=False)
     plot(range(len(loss_record)), 
          [record[0] for record in loss_record], 
          [record[1] for record in loss_record], 
-         ["segements loss", "full model loss", "losses"], loss=True)
+         ["segements loss", "full model loss", "train_full_losses"], loss=True)
 
 if __name__ == "__main__":
-    main()
+    x = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1).astype(np.float32) 
+    y = np.sin(x).reshape(-1, 1).astype(np.float32)  
+    args = {
+        'x' : x, 
+        'y' : y,
+        'layers_per_segment':2,
+        'number_of_segments':2,
+        'units':16,
+        'epochs':5000,
+        'learning_rate':0.001,
+        'train_full_model': False
+    }
+    main(args)
